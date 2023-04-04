8970bfcb7ac5b120b6b8ac7d23f5cce1867992c3
Jarrett Guyer <guyer.jarrett@gmail.com>
2023-04-03 19:16:17 -0600

    rotating a webgl canvas distorts image, removed rotation

--- src/pages/Logico/index.jsx
+++ src/pages/Logico/index.jsx
@@ -1,5 +1,5 @@
 import { Unity, useUnityContext } from 'react-unity-webgl';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from '@/styles/logico.module.css';
 
 export default function Logico() {
@@ -10,32 +10,17 @@ export default function Logico() {
     codeUrl: '/Build/Builds.wasm'
   });
 
-  const [devicePixelRatio, setDevicePixelRatio] = useState();
-
   useEffect(() => {
     return () => {
       detachAndUnloadImmediate().catch((reason) => {
         console.log(reason);
       });
-      setDevicePixelRatio(window.devicePixelRatio);
     };
   }, [detachAndUnloadImmediate]);
 
-  useEffect(() => {
-    setDevicePixelRatio(window.devicePixelRatio);
-  }, []);
-
-  useEffect;
-
   return (
     <div className={styles.wrapper}>
-      <Unity
-        width={640}
-        height={360}
-        className={styles.player}
-        unityProvider={unityProvider}
-        devicePixelRatio={devicePixelRatio}
-      />
+      <Unity className={styles.player} unityProvider={unityProvider} />
     </div>
   );
 }
