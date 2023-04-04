131c97d07db4fe609d3bfaaa333c15958e90da21
Jarrett Guyer <guyer.jarrett@gmail.com>
2023-04-03 18:48:16 -0600

    turn game when screen is thin

--- src/pages/Logico/index.jsx
+++ src/pages/Logico/index.jsx
@@ -1,16 +1,13 @@
-
-import { Unity, useUnityContext } from "react-unity-webgl";
-import {useEffect} from 'react'
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import { useEffect } from 'react';
+import styles from '@/styles/logico.module.css';
 
 export default function Logico() {
-  const {
-    unityProvider,
-    UNSAFE__detachAndUnloadImmediate: detachAndUnloadImmediate,
-  } = useUnityContext({
-    loaderUrl: "/Build/Builds.loader.js",
-    dataUrl: "/Build/Builds.data",
-    frameworkUrl: "/Build/Builds.framework.js",
-    codeUrl: "/Build/Builds.wasm",
+  const { unityProvider, UNSAFE__detachAndUnloadImmediate: detachAndUnloadImmediate } = useUnityContext({
+    loaderUrl: '/Build/Builds.loader.js',
+    dataUrl: '/Build/Builds.data',
+    frameworkUrl: '/Build/Builds.framework.js',
+    codeUrl: '/Build/Builds.wasm'
   });
 
   useEffect(() => {
@@ -22,12 +19,8 @@ export default function Logico() {
   }, [detachAndUnloadImmediate]);
 
   return (
-    <div style={{width: '100%', height: '100%',display:'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <Unity
-      width={640} height={360}
-        style={{ width: "640px", height: "360px",outline: '4px solid white', boxSizing: 'border-box'}}
-        unityProvider={unityProvider}
-      />
+    <div className={styles.wrapper}>
+      <Unity width={640} height={360} className={styles.player} unityProvider={unityProvider} />
     </div>
   );
-};
+}
